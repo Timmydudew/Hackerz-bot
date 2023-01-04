f88757b55314754511291ea32fcff8f216890a14
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2023-01-04 19:45:23 -0200

    Create index.js

---
+++ julie/index.js
@@ -0,0 +1,16 @@
+/* Copyright (C) 2022 JT.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Thanks to WhatsAsena - Yusuf Usta
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
